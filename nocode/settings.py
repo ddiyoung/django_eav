734da484c0fcbf00fee4484ade476539bbcea336
ddiyoung <wsw3998@sju.ac.kr>
2025-02-18 16:49:31 +0900

    chore(settings.py) : REST_FRAMEWORK.DEFAULT_PERMISSION_CLASSES 추가

--- nocode/settings.py
+++ nocode/settings.py
@@ -99,13 +99,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
