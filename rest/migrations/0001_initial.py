b516bdc44e6b6b71139b17f9ad561970559583ae
ddiyoung <wsw3998@sju.ac.kr>
2025-02-18 19:28:05 +0900

    migrate : Table 변경

--- rest/migrations/0001_initial.py
+++ rest/migrations/0001_initial.py
@@ -5,28 +5,74 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Table',
+            name="Table",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=255)),
             ],
         ),
         migrations.CreateModel(
-            name='Column',
+            name="Column",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=255)),
-                ('column_type', models.CharField(choices=[('BooleanField', 'BooleanField'), ('CharField', 'CharField'), ('DateField', 'DateField'), ('DecimalField', 'DecimalField'), ('DurationField', 'DurationField'), ('FilePathField', 'FilePathField'), ('FloatField', 'FloatField'), ('IntegerField', 'IntegerField'), ('IPAddressField', 'IPAddressField'), ('GenericIPAddressField', 'GenericIPAddressField'), ('TextField', 'TextField'), ('TimeField', 'TimeField'), ('BinaryField', 'BinaryField'), ('UUIDField', 'UUIDField'), ('JSONField', 'JSONField'), ('FileField', 'FileField'), ('GeneratedField', 'GeneratedField'), ('RelatedField', 'RelatedField')], max_length=50)),
-                ('is_nullable', models.BooleanField(default=True)),
-                ('table', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='columns', to='rest.table')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=255)),
+                (
+                    "column_type",
+                    models.CharField(
+                        choices=[
+                            ("BooleanField", "BooleanField"),
+                            ("CharField", "CharField"),
+                            ("DateField", "DateField"),
+                            ("DecimalField", "DecimalField"),
+                            ("DurationField", "DurationField"),
+                            ("FilePathField", "FilePathField"),
+                            ("FloatField", "FloatField"),
+                            ("IntegerField", "IntegerField"),
+                            ("IPAddressField", "IPAddressField"),
+                            ("GenericIPAddressField", "GenericIPAddressField"),
+                            ("TextField", "TextField"),
+                            ("TimeField", "TimeField"),
+                            ("BinaryField", "BinaryField"),
+                            ("UUIDField", "UUIDField"),
+                            ("JSONField", "JSONField"),
+                            ("FileField", "FileField"),
+                            ("GeneratedField", "GeneratedField"),
+                            ("RelatedField", "RelatedField"),
+                        ],
+                        max_length=50,
+                    ),
+                ),
+                ("is_nullable", models.BooleanField(default=True)),
+                (
+                    "table",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name="columns",
+                        to="rest.table",
+                    ),
+                ),
             ],
         ),
     ]
