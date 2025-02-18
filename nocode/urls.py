ef1c54d108d2dad431aa548a4f4480504f0ff11d
ddiyoung <wsw3998@sju.ac.kr>
2025-02-18 20:17:40 +0900

    refactor : views 디렉토리 생성 및 기능 별 view 분리

--- nocode/urls.py
+++ nocode/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from rest.views import SignInView, SignUpView
+from rest.views.signviews import SignInView, SignUpView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
